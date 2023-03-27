78ae02dd6337835acce7c41586144f3ec25bc16e
Faaiz <faaizaslam75@live.com>
2023-03-27 19:19:31 +0500

    Init Project

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import color from './src/constants/colors';
+import {
+	useFonts,
+	Inter_100Thin,
+	Inter_200ExtraLight,
+	Inter_300Light,
+	Inter_400Regular,
+	Inter_500Medium,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_800ExtraBold,
+	Inter_900Black
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+		Inter_100Thin,
+		Inter_200ExtraLight,
+		Inter_300Light,
+		Inter_400Regular,
+		Inter_500Medium,
+		Inter_600SemiBold,
+		Inter_700Bold,
+		Inter_800ExtraBold,
+		Inter_900Black,
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text style={{fontFamily:'Inter_400Regular'}}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        
+      </View>
+    </PaperProvider>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: color.yellow,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+AppRegistry.registerComponent(appName, () => Main);
+
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+const color = {
+	grey:'#2D3748',
+	yellow: '#FFC727'
+}
+
+export default color
